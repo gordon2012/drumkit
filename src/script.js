a2e8b599532d495c59d564d7e2703841a2c162d8
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-01-21 16:32:32 -0800

    Implement drums (Closes #7)

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
-document.querySelector("button").addEventListener("click", e => { console.log('BA-DUM!'); });
+// Click event for each drum
+//
+const drums = document.querySelectorAll('.drum');
+drums.forEach(d => {
+    d.addEventListener('click', e => {
+        const audio = document.querySelector(`audio[data-key="${d.dataset.key}"]`);
+
+        audio.currentTime = 0;
+        audio.play();
+    });
+});
+
+// Keyboard Input
+//
+document.addEventListener('keydown', e => {
+    const drum = document.querySelector(`.drum[data-key="${e.keyCode}"]`);
+
+    if(!drum) return;
+    drum.click();
+});
